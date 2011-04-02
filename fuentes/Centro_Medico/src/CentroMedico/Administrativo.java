2a986c32037e66ebb878cc001d3e2ad9817c2943
fjbrion@gmail.com <fjbrion@gmail.com@08e3380e-38ca-6e33-764e-4eba121d5b77>
2011-04-02 16:22:43 +0000

    Nuevas Clases

---
+++ fuentes/Centro_Medico/src/CentroMedico/Administrativo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package CentroMedico;
+
+/**
+ *
+ * @author pyrus
+ */
+public class Administrativo {
+
+}
