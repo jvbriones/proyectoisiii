49618a7415f45eed0e28e6e1aa795bb050d7c7ce
jcbs.pro@gmail.com <jcbs.pro@gmail.com@08e3380e-38ca-6e33-764e-4eba121d5b77>
2011-05-02 23:06:16 +0000

    RadiografiaBD modificado

--- fuentes/Segunda_Iteracion/Centro_Medico/src/BaseDatos/RadiografiaBD.java
+++ fuentes/Segunda_Iteracion/Centro_Medico/src/BaseDatos/RadiografiaBD.java
@@ -4,14 +4,43 @@
  */
 
 package BaseDatos;
+import CentroMedico.Radiografia;
+import org.hibernate.Session;
 
 /**
  *
  * @author pope
  */
 public class RadiografiaBD {
-    void almacenar(Radiografia Rad){}
-    Radiografia obtener (int Id){}
-    void actualizar (Radiografia Rad){}
-    void eliminar (Radiografia Rad){}
+    void almacenar(Radiografia Rad){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+
+        session.beginTransaction ();
+        session.save ( Rad );
+
+    }
+
+    Radiografia obtener (int Id){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+
+        session.beginTransaction ();
+
+        Radiografia Rad = (Radiografia) session.get(Radiografia.class, Id);
+
+        return Rad;
+    }
+
+    void actualizar (Radiografia Rad){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+
+        session.beginTransaction ();
+        session.update ( Rad );    
+    }
+
+    void eliminar (Radiografia Rad){
+    Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+
+        session.beginTransaction ();
+        session.delete ( Rad );
+    }
 }
