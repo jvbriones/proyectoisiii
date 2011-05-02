d939162300df6cc61b0f6ad075a5bd552c293808
jcbs.pro@gmail.com <jcbs.pro@gmail.com@08e3380e-38ca-6e33-764e-4eba121d5b77>
2011-05-02 22:43:21 +0000

    LoteMedicamentoBD implementado

---
+++ fuentes/Segunda_Iteracion/Centro_Medico/src/BaseDatos/LoteMedicamentoBD.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package BaseDatos;
+import CentroMedico.*;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author pope
+ */
+public class LoteMedicamentoBD {
+    public void almacenar(LoteMedicamento Lote){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+
+        session.beginTransaction ();
+        session.save(Lote);
+    }
+
+    public LoteMedicamento obtener (String CodBarras){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+
+        session.beginTransaction ();
+        LoteMedicamento Lote = (LoteMedicamento) session.get(LoteMedicamento.class, CodBarras);
+        return Lote;
+    }
+
+    public void actualizar (LoteMedicamento Lote){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+
+        session.beginTransaction ();
+        session.update(Lote);
+    }
+
+    public void eliminar (LoteMedicamento Lote){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+
+        session.beginTransaction ();
+        session.delete(Lote);
+    }
+
+    public void eliminarLote (String CodBarras){
+        LoteMedicamento Lote = new LoteMedicamento();
+        Lote = obtener (CodBarras);
+
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+
+        session.beginTransaction ();
+        session.delete(Lote);
+    }
+}
