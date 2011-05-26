7cd26c84526dd7675d9fc8b350055a3a40d17c82
juanmiguel.omega@gmail.com <juanmiguel.omega@gmail.com@08e3380e-38ca-6e33-764e-4eba121d5b77>
2011-05-26 12:20:44 +0000

    Que nadie toque las CITAS! Estoy con trabajando con ellas.

--- fuentes/TerceraIteracion/Centro_Medico/src/BaseDatos/CitasBD.java
+++ fuentes/TerceraIteracion/Centro_Medico/src/BaseDatos/CitasBD.java
@@ -1,8 +1,16 @@
+/*
+ * CitasBD
+ *
+ * Mayo 2011
+ *
+ * Sub-equipo 3
+ */
+
 package BaseDatos;
 
 import CentroMedico.Cita;
 import java.sql.*;
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import org.hibernate.*;
 
 public class CitasBD {
@@ -12,90 +20,36 @@ public class CitasBD {
     String pass = "generico";
 
     /*
-     * existencia
-     * Comprueba si existen citas para un paciente concreto.
-     * HAY QUE CAMBIARLA A HIBERNATE
-     *
-     * Miguel-> Juanmi, Ya está cambiada
-     */
-    public boolean existeCita(String Dni) throws SQLException {
-         Cita ci = obtenerCita(Dni);
-
-        if (ci != null){
-            System.out.print("ExisteCita: Existe la Cita");
-           return true;
-       }
-       else{
-           System.out.print( "ExisteCita: No existe la cita\n");
-           return false;
-       }
-    }
-
-    /*
      * almacenarCita
      * Dado un objeto Cita, lo almacena en la base de datos.
-     * HAY QUE CAMBIARLA A HIBERNATE
-     *
-     * Miguel-> Juanmi, Ya está cambiada
+     * @param cita, Objeto Cita a almacenar
+     * @post No ofrece información sobre el estado de la operacion
      */
 
     public void almacenar(Cita cita){
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-
-        session.beginTransaction ();
-        session.save ( cita );
+        
+        session.beginTransaction();
+        session.save(cita);
         session.getTransaction().commit();
     }
- 
 
+    
     /*
      * obtenerCita
      * Obtiene la cita relativa a un usuario dado.
-     * HAY QUE CAMBIARLA A HIBERNATE
-     * Miguel.  Ya está cambiada
+     * @param Dni, Identificación del paciente del que obtener la cita
+     * @return El objeto Cita oportuno
      */
     public Cita obtenerCita(String Dni) throws SQLException {
-
-         
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+
         session.beginTransaction();
         Cita ci = (Cita) session.get(Cita.class, Dni);
+
         return ci;
-    
     }
 
-    /*  (OPCIONAL)
-     * EstadisticasCitas
-     * Muestra las estadisticas relativas a las citas
-     * HABRIA QUE CAMBIARLA A HIBERNATE
-   
-    public ArrayList EstadisticasCitas(Date fecha_inicio, Date fecha_fin) throws SQLException {
-        ArrayList datosAdmin = new ArrayList();
-
-        Jdbc conexion = new Jdbc();
-        conexion.doConnection(IpDelServidor, NombreDB, user,pass);
-        String Consulta = "SELECT * FROM Citas WHERE "+fecha_inicio+"<Fecha AND Fecha<"+fecha_fin;
-        ResultSet rs = conexion.consultaSelect(Consulta);
-        while(rs.next()) {
-            String dni=rs.getString("DniPaciente");
-            String dniM=rs.getString("DniMedico");
-            Date fecha=rs.getDate("Fecha");
-            boolean estado=rs.getBoolean("Estado");
-            String dniA=rs.getString("DniAdministrativoCita");
-
-            datosAdmin.add(dni);
-            datosAdmin.add(dniM);
-            datosAdmin.add(fecha);
-            datosAdmin.add(estado);
-            datosAdmin.add(dniA);
-
-        }
-        return datosAdmin;
-    }
-     *   */
-     
-    
-   
 
     /*
      * actualizar
@@ -123,4 +77,54 @@ public class CitasBD {
         session.delete (ci);
         session.getTransaction().commit();
     }
+
+    /*
+     * existencia
+     * Comprueba si existen citas para un paciente concreto.
+     */
+    public boolean existeCita(String Dni) throws SQLException {
+        Cita cit = obtenerCita(Dni);
+
+        if (cit != null){
+            System.out.print("ExisteCita: Existe la Cita");
+           return true;
+       }
+       else{
+           System.out.print("ExisteCita: No existe la cita");
+           return false;
+       }
+    }
+
+
+    /*  (OPCIONAL)
+     * EstadisticasCitas
+     * Muestra las estadisticas relativas a las citas
+     * HABRIA QUE CAMBIARLA A HIBERNATE
+
+    public ArrayList EstadisticasCitas(Date fecha_inicio, Date fecha_fin) throws SQLException {
+        ArrayList datosAdmin = new ArrayList();
+
+        Jdbc conexion = new Jdbc();
+        conexion.doConnection(IpDelServidor, NombreDB, user,pass);
+        String Consulta = "SELECT * FROM Citas WHERE "+fecha_inicio+"<Fecha AND Fecha<"+fecha_fin;
+        ResultSet rs = conexion.consultaSelect(Consulta);
+        while(rs.next()) {
+            String dni=rs.getString("DniPaciente");
+            String dniM=rs.getString("DniMedico");
+            Date fecha=rs.getDate("Fecha");
+            boolean estado=rs.getBoolean("Estado");
+            String dniA=rs.getString("DniAdministrativoCita");
+
+            datosAdmin.add(dni);
+            datosAdmin.add(dniM);
+            datosAdmin.add(fecha);
+            datosAdmin.add(estado);
+            datosAdmin.add(dniA);
+
+        }
+        return datosAdmin;
+    }
+     *   */
+
+
 }
