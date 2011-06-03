25fb12de3be13ae385a3a307aeda0fbd1d228dbc
fadoro@gmail.com <fadoro@gmail.com@08e3380e-38ca-6e33-764e-4eba121d5b77>
2011-06-03 16:17:57 +0000

    restaurado bug

--- fuentes/TerceraIteracion/Centro_Medico/src/Interfaz/UI_Administrador.java
+++ fuentes/TerceraIteracion/Centro_Medico/src/Interfaz/UI_Administrador.java
@@ -2233,15 +2233,7 @@ public class UI_Administrador extends javax.swing.JFrame {
             if(Character.isDigit(teclaPresionada) == false)
                 evt.consume();
 
-        if(jTextFieldTelefonoPaciente.getText().length() == 9){
-            if(Character.isDigit(teclaPresionada) == true){
-                evt.consume();
-            }else{
-                evt.consume();
-            }
-        }
-
-        if(jTextFieldTelefonoPaciente.getText().length() > 9)
+        if(jTextFieldTelefonoPaciente.getText().length() >= 9)
             evt.consume();
     }//GEN-LAST:event_jTextFieldTelefonoPacienteKeyTyped
 
@@ -2276,11 +2268,19 @@ public class UI_Administrador extends javax.swing.JFrame {
 
         /**Limitamos a que el DNI siempre sea correcto sintácticamente*/
         char teclaPresionada = evt.getKeyChar();
-        if(jTextFieldTelefonoPaciente.getText().length() < 9)
+
+        if(jTextFieldDNIPaciente.getText().length() < 8)
             if(Character.isDigit(teclaPresionada) == false)
                 evt.consume();
 
-        if(jTextFieldTelefonoPaciente.getText().length() >= 9)
+        if(jTextFieldDNIPaciente.getText().length() == 8)
+            if(Character.isDigit(teclaPresionada) == true){
+                evt.consume();
+            }else if(Character.isLowerCase(teclaPresionada)){
+                jTextFieldDNIPaciente.setText(jTextFieldDNIPersonal.getText().toUpperCase());
+            }
+
+        if(jTextFieldDNIPersonal.getText().length() > 8)
             evt.consume();
             
     }//GEN-LAST:event_jTextFieldDNIPacienteKeyTyped
