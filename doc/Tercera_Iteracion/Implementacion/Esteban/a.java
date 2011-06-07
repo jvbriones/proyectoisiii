2692001bf0f8f92413a01f5609b27fdfe4933e6b
fadoro@gmail.com <fadoro@gmail.com@08e3380e-38ca-6e33-764e-4eba121d5b77>
2011-06-07 04:31:13 +0000

    Aniadido un programita para poder codificar claves con AES

--- doc/Tercera_Iteracion/Implementacion/Esteban/a.java
+++ doc/Tercera_Iteracion/Implementacion/Esteban/a.java
@@ -11,13 +11,47 @@ import java.util.*;
 public class a{
 	public static void main(String[] args) throws Exception {
 
-
+/*
 		String mode = "encrypt";
-		String text = "admi";
-		//String cadena = "XxO5RiI4J883iEFsiiYnHQ==";
-		System.out.print("\nCadena encriptar: "+"nCadena codificada: "+AES.encrypt(text)+"\n");
-		//System.out.print("\nCadena desencriptar: "+cadena+"\nCadena decodificada: "+as.decrypt(cadena)+"\n");
+		String text = "as";
+		String cadena = "LJYejAf7fUE8a9ZW5t+1vQ==";
+		System.out.print("\nCadena encriptar: "+text+"\nCadena codificada: "+AES.encrypt(text)+"\n");
+		System.out.print("\nCadena desencriptar: "+cadena+"\nCadena decodificada: "+AES.decrypt(cadena)+"\n");
 		//as.decrypt(cadena);
+	*/	
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String text = "as";
+		String cad;
+		Boolean salir=false;
+		System.out.println("**********************************************************************");
+        System.out.println("**** Programa para obtener una clave cifrada usando AES 128 bits.*****");
+        while(!salir){
+			System.out.println("\n\tOpciones del programa:\n\n\t\t[1]: Imprimir clave predefinida\n\t\t[2]: Obtener clave introducia\n\t\t[0]: Salir\n\n");
+			System.out.print("Elige: ");
+			 cad = in.readLine();
+			
+			switch (Integer.parseInt(cad)){
+				case 1:
+					System.out.print("\nCadena encriptar: "+text+"\nCadena codificada: "+AES.encrypt(text)+"\n");
+					System.out.println("_______________________________");
+					break;
+				case 2:
+				    System.out.print("\nIntroduce una clave: ");
+				    cad = in.readLine();
+				    System.out.print("\nCadena encriptar: "+cad+"\nCadena codificada: "+AES.encrypt(cad)+"\n");
+				    System.out.println("_______________________________");
+				    break;
+				case 0:
+				    salir=true;
+				    System.out.println("Saliendo...");
+				    break;
+			};
+			
+		}
+        
+		
+
+		
 	}
 }
 
